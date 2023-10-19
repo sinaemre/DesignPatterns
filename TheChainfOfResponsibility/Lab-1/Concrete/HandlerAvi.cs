9a1425f60701900d71f02c0b30c5695fc9574ccb
sinaemre <snabkr7010@gmail.com>
2023-10-19 10:58:33 +0300

    Last-Update-19-10-2023

---
+++ TheChainfOfResponsibility/Lab-1/Concrete/HandlerAvi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheChainfOfResponsibility.Lab_1.Abstract;
+
+namespace TheChainfOfResponsibility.Lab_1.Concrete
+{
+    public class HandlerAvi : PlayerHandler
+    {
+        public override void Player(string fileType)
+        {
+            if (fileType.EndsWith(".avi"))
+                Console.WriteLine($"{fileType} tipinde medya oynatılıyor...");
+            else
+            {
+                Console.WriteLine("Geçersiz medya tipi!");
+            }
+        }
+    }
+}
---
+++ TheChainfOfResponsibility/Program.cs
@@ -0,0 +1,27 @@
+﻿using TheChainfOfResponsibility.Lab_1.Abstract;
+using TheChainfOfResponsibility.Lab_1.Concrete;
+
+namespace TheChainfOfResponsibility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zincirin halkalarını tanımlayalaım.
+            PlayerHandler aviPlayer = new HandlerAvi();
+            PlayerHandler mp3Player = new HandlerMP3();
+            PlayerHandler mp4Player = new HandlerMP4();
+
+            //Zinciri oluşturuyoruz
+            mp3Player.NextHandler = mp4Player;
+            mp4Player.NextHandler = aviPlayer;
+
+            //İstekleri zincirin ilk halkasına gönderiyoruz.
+            mp3Player.Player("video.mpg");
+            mp3Player.Player("video.avi");
+            mp3Player.Player("video.mp3");
+            mp3Player.Player("video.mp4");
+        }
+
+    }
+}
